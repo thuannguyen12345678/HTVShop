160f7102629ebbda6003df80f8ac87e03b336878
Ngocvinh2809 <vinhtrann2809@gmail.com>
2022-10-25 23:04:51 +0700

    model-vv

--- BackEnd/app/Models/Customer.php
+++ BackEnd/app/Models/Customer.php
@@ -10,7 +10,7 @@ class Customer extends Model
     use HasFactory;
     protected $table = 'customers';
     function order(){
-        return $this->belongsTo(Course::class);
+        return $this->belongsTo(Order::class);
     }
     function province(){
         return $this->hasMany(Province::class);
