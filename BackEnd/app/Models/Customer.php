8550948f6103c9c88ac70cfa694fd987e5bbb186
Ngocvinh2809 <vinhtrann2809@gmail.com>
2022-10-25 22:55:26 +0700

    model-v

---
+++ BackEnd/app/Models/Customer.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    use HasFactory;
+    protected $table = 'customers';
+    function order(){
+        return $this->belongsTo(Course::class);
+    }
+    function province(){
+        return $this->hasMany(Province::class);
+    }
+    function district(){
+        return $this->hasMany(District::class);
+    }
+    function ward(){
+        return $this->hasMany(Ward::class);
+    }
+}
