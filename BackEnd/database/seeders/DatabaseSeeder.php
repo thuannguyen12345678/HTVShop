d5b6d4811a214732a3473c9ef354f5be8215d1bc
unknown <haipro2141999@gmail.com>
2022-10-25 22:07:31 +0700

    Role_group migrate seeder

--- BackEnd/database/seeders/DatabaseSeeder.php
+++ BackEnd/database/seeders/DatabaseSeeder.php
@@ -26,6 +26,7 @@ class DatabaseSeeder extends Seeder
             CustomerSeeder::class,
             OrderSeeder::class,
             OrderDetailSeeder::class,
+            RoleGroupSeeder::class,
 
         ]);
     }
