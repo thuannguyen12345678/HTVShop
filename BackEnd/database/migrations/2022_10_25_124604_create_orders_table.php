4b577977a45e9b6e5fb08350c7125abc84e2db23
Ngocvinh2809 <vinhtrann2809@gmail.com>
2022-10-25 20:08:30 +0700

    migrate-v

---
+++ BackEnd/database/migrations/2022_10_25_124604_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('note');
+            $table->string('address');
+            $table->string('phone');
+            $table->integer('order_total_price');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
