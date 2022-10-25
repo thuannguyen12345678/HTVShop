f465d1e5031832f96d9c51adc8f38c68cdcf4864
unknown <haipro2141999@gmail.com>
2022-10-25 22:45:28 +0700

    user validate

---
+++ BackEnd/app/Http/Requests/Store/StoreUserRequest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Requests\Store;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+            return [
+                'email' => 'required|unique:users|email',
+                'name' => 'required',
+                'address' => 'required',
+                'phone' => 'required|number',
+                'address' => 'required',
+                'day_of_birth' => 'required',
+                'avatar' => 'required',
+                'group_id'=>'required'
+            ];
+    }
+    public function messages()
+    {
+    return [
+        'email.required' => 'Trường bắt buộc',
+        'name.required' => 'Trường bắt buộc',
+        'address.required' => 'Trường bắt buộc',
+        'phone.required' => 'Trường bắt buộc',
+        'address.required' => 'Trường bắt buộc',
+        'day_of_birth.required' => 'Trường bắt buộc',
+        'avatar.required' => 'Trường bắt buộc',
+        'group_id.required' => 'Trường bắt buộc',
+        'email.unique' => 'Email đã bị trùng',
+        'email.email' => 'Email không đúng định dạng',
+    ];
+    }
+}
