59543f6ff01eee6961f3a726c904d23dbde00304
thuannguyen <nguyenducthuan0303@gmail.com>
2022-11-02 16:56:15 +0700

    update_api

--- FrontEnd/src/environments/environment.ts
+++ FrontEnd/src/environments/environment.ts
@@ -1,9 +1,11 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
+let urlApi = 'http://127.0.0.1:8000/api/';
 export const environment = {
-  production: false
+  production: false,
+  url:'http://127.0.0.1:8000/',
+  urlAllProducts : urlApi+'product_list',
 };
 
 /*
